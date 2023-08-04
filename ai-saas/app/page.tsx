76861f298245eba501f130ffc2e276876e45f6d1
Suryansh Jain <suryanshjain531@gmail.com>
2023-08-04 19:13:38 +0530

    Next / shadcn-ui setup
    NextJS setup, shadcn ui setup
    Homepage clean-up

---
+++ ai-saas/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+
+      <p className="text-6xl text-green-500">AI SaaS</p>
+    </div>
+  );
+}
