c96380f549876ba99acedae8c260cfb0e55cedc8
Suryansh Jain <suryanshjain531@gmail.com>
2023-08-04 19:29:39 +0530

    Dashboard and Landing page route setup

---
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+    return (
+      <div>
+        <p className="text-6xl text-green-500">Landing</p>
+      </div>
+    );
+  };
+  
+  export default LandingPage;
+  
