dc8ca8ba49b2d613ad39e3c5465050d78d4c46fa
Suryansh Jain <suryanshjain531@gmail.com>
2023-09-10 01:02:28 +0530

    Changed subscription price

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -46,7 +46,7 @@ export async function GET() {
               name: "Genius Pro",
               description: "Unlimited AI Generations"
             },
-            unit_amount: 2000,
+            unit_amount: 200000,
             recurring: {
               interval: "month"
             }
